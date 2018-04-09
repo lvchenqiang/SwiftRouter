017ed8d70eb8c47260e2114b7c06fc2e8383c646
吕陈强 <lvchenqiang@ellabook.cn>
2018-04-09 18:27:35 +0800

    完善代码

--- SwiftRouter/Router/Category/String+Extension/String+Extension.swift
+++ SwiftRouter/Router/Category/String+Extension/String+Extension.swift
@@ -101,3 +101,93 @@ extension String{
     }
     
 }
+
+
+
+// MARK:搜索str所有所在位置的集合
+extension String {
+    
+        func indexesOf(pattern: String) -> [Int]? {
+            let patternLength: Int = pattern.characters.count
+            /* Let's calculate the Z-Algorithm on the concatenation of pattern and text */
+            let zeta = ZetaAlgorithm(ptrn: pattern + "💲" + self)
+            
+            guard zeta != nil else {
+                return nil
+            }
+            
+            var indexes: [Int] = [Int]()
+            
+            /* Scan the zeta array to find matched patterns */
+            for i in 0 ..< zeta!.count {
+                if zeta![i] == patternLength {
+                    indexes.append(i - patternLength - 1)
+                }
+            }
+            
+            guard !indexes.isEmpty else {
+                return nil
+            }
+            
+            return indexes
+        }
+}
+
+
+
+func ZetaAlgorithm(ptrn: String) -> [Int]? {
+    
+    let pattern = Array(ptrn.characters)
+    let patternLength: Int = pattern.count
+    
+    guard patternLength > 0 else {
+        return nil
+    }
+    
+    var zeta: [Int] = [Int](repeating: 0, count: patternLength)
+    
+    var left: Int = 0
+    var right: Int = 0
+    var k_1: Int = 0
+    var betaLength: Int = 0
+    var textIndex: Int = 0
+    var patternIndex: Int = 0
+    
+    for k in 1 ..< patternLength {
+        if k > right {  // Outside a Z-box: compare the characters until mismatch
+            patternIndex = 0
+            
+            while k + patternIndex < patternLength  &&
+                pattern[k + patternIndex] == pattern[patternIndex] {
+                    patternIndex = patternIndex + 1
+            }
+            
+            zeta[k] = patternIndex
+            
+            if zeta[k] > 0 {
+                left = k
+                right = k + zeta[k] - 1
+            }
+        } else {  // Inside a Z-box
+            k_1 = k - left + 1
+            betaLength = right - k + 1
+            
+            if zeta[k_1 - 1] < betaLength { // Entirely inside a Z-box: we can use the values computed before
+                zeta[k] = zeta[k_1 - 1]
+            } else if zeta[k_1 - 1] >= betaLength { // Not entirely inside a Z-box: we must proceed with comparisons too
+                textIndex = betaLength
+                patternIndex = right + 1
+                
+                while patternIndex < patternLength && pattern[textIndex] == pattern[patternIndex] {
+                    textIndex = textIndex + 1
+                    patternIndex = patternIndex + 1
+                }
+                
+                zeta[k] = patternIndex - k
+                left = k
+                right = patternIndex - 1
+            }
+        }
+    }
+    return zeta
+}
